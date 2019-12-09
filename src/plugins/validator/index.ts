4262f369bf9c446d715f0802adc565411e94eed0
Han <c24345173@starlordtech.com>
2019-12-09 18:19:19 +0800

    add rules

--- src/plugins/validator/index.ts
+++ src/plugins/validator/index.ts
@@ -1,5 +1,6 @@
 import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
 import { required, email } from 'vee-validate/dist/rules'
+import { params } from 'vee-validate/dist/types/rules/alpha'
 
 export default {
   install (Vue:any) {
@@ -19,6 +20,18 @@ export default {
     extend('mobile', {
       validate: value => value.length === 10 && /^09\d{8}$/.test(value),
       message: '請輸入正確格式'
+    })
+
+    extend('min-charts', {
+      validate: (value, args) => value.length >= args.length,
+      params: ['length'],
+      message: '請輸入最少 {length} 個字'
+    })
+
+    extend('minmax-charts', {
+      validate: (value, { min, max }:any) => value.length >= min && value.length <= max,
+      params: ['min', 'max'],
+      message: '請輸入 {min} 到 {max} 個字'
     })
   }
 }
