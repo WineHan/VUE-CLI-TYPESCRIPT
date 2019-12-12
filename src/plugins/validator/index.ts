a2dfbccf2721ab7c6ff6dfb39027144178e3dc05
Han <c24345173@starlordtech.com>
2019-12-12 13:16:58 +0800

    combine vee-validate and i18n message

--- src/plugins/validator/index.ts
+++ src/plugins/validator/index.ts
@@ -1,37 +1,24 @@
 import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
-import { required, email } from 'vee-validate/dist/rules'
-import { params } from 'vee-validate/dist/types/rules/alpha'
-
+import { required, email, min } from 'vee-validate/dist/rules'
 export default {
   install (Vue:any) {
     Vue.component('ValidationProvider', ValidationProvider)
     Vue.component('ValidationObserver', ValidationObserver)
 
     extend('required', {
-      ...required,
-      message: '必填'
+      ...required
     })
 
     extend('email', {
-      ...email,
-      message: '請輸入正確格式'
+      ...email
     })
 
     extend('mobile', {
-      validate: value => value.length === 10 && /^09\d{8}$/.test(value),
-      message: '請輸入正確格式'
+      validate: value => value.length === 10 && /^09\d{8}$/.test(value)
     })
 
-    extend('min-charts', {
-      validate: (value, args) => value.length >= args.length,
-      params: ['length'],
-      message: '請輸入最少 {length} 個字'
-    })
-
-    extend('minmax-charts', {
-      validate: (value, { min, max }:any) => value.length >= min && value.length <= max,
-      params: ['min', 'max'],
-      message: '請輸入 {min} 到 {max} 個字'
+    extend('min', {
+      ...min
     })
   }
 }
